1cb014df603ab8c31bb8d28371b2fd3a2f9f27e8
hegnemurd <hegne.murd@gmail.com>
2021-04-09 10:43:50 +0100

    Refactoring JavaScript Promises with Async/Await

--- javascript-mainly/refactoring-javascript-promises-with-async-await/src/index.js
+++ javascript-mainly/refactoring-javascript-promises-with-async-await/src/index.js
@@ -9,3 +9,21 @@ const getBooksAndMovies = () => {
     }))
     .catch((error) => console.log('Error fetching books and movies', error));
 };
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then((results) => {
+  console.log('getBooksAndMoviesPromise', results);
+});
+
+function getBooksOrMovies() {
+  return Promise.race([fetchBooks(), fetchMovies()])
+    .then((results) => results)
+    .catch((error) => {
+      console.log('Error waiting for the promise race', error);
+    });
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then((results) => {
+  console.log('getBooksOrMoviesPromise', results);
+});
